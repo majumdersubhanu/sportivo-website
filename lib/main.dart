0ab76e802909e1baa806b5050182fb7b35c59c53
Subhanu Majumder <reach.subhanu@gmail.com>
2024-03-02 13:28:31 +0530

    carousel added for upcoming events

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:gap/gap.dart';
 import 'package:glassmorphism_ui/glassmorphism_ui.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:neopop/neopop.dart';
-import 'package:neopop/widgets/buttons/neopop_button/neopop_button.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,9 +17,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Sportivo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData(useMaterial3: false).copyWith(
+        textTheme: GoogleFonts.poppinsTextTheme(),
       ),
       home: const MyHomePage(),
     );
@@ -65,92 +64,222 @@ class _MyHomePageState extends State<MyHomePage> {
             decoration: const BoxDecoration(),
             child: Stack(
               children: [
-                Container(
-                  transform: Matrix4.identity()
-                    ..translate(
-                      0.0,
-                      controller.hasClients
-                          ? (-(index * MediaQuery.of(context).size.height) +
-                                  controller.position.pixels) /
-                              2
-                          : 0.0,
-                    ),
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  child: Image.asset(
-                    'assets/images/sportivo_backdrop${index + 1}.jpg',
-                    fit: BoxFit.cover,
-                  ),
+                ParallaxBackdrop(
+                  controller: controller,
+                  imageUrl: 'assets/images/sportivo_backdrop${index + 1}.jpg',
+                  index: index,
                 ),
-                Center(
-                  child: GlassContainer(
-                    height: MediaQuery.of(context).size.height / 2.5,
-                    width: MediaQuery.of(context).size.width / 2,
-                    blur: 4,
-                    child: Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        textBaseline: TextBaseline.ideographic,
-                        children: [
-                          Column(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                texts[index],
-                                style: const TextStyle(
-                                  fontSize: 40.0,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w900,
-                                ),
-                              ),
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width / 4,
-                                child: const Text(
-                                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-                                  overflow: TextOverflow.ellipsis,
-                                  maxLines: 5,
-                                  softWrap: true,
-                                  style: TextStyle(
-                                    fontSize: 20.0,
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.normal,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                          const SizedBox(height: 20),
-                          NeoPopTiltedButton(
-                            isFloating: true,
-                            color: Colors.white,
-                            onTapUp: () => HapticFeedback.vibrate(),
-                            onTapDown: () => HapticFeedback.vibrate(),
-                            floatingDuration: const Duration(milliseconds: 500),
-                            child: const Padding(
-                              padding: EdgeInsets.symmetric(
-                                  horizontal: 20, vertical: 15),
-                              child: Text(
-                                "Call To Action",
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                HomePageSection(
+                  header: texts[index],
+                  content: index == 2 ? const UpcomingEventsSlider() : null,
                 ),
               ],
             ),
           );
         },
       ),
+    );
+  }
+}
+
+class ParallaxBackdrop extends StatelessWidget {
+  final String imageUrl;
+  final int index;
+
+  const ParallaxBackdrop({
+    super.key,
+    required this.controller,
+    required this.imageUrl,
+    required this.index,
+  });
+
+  final ScrollController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      transform: Matrix4.identity()
+        ..translate(
+          0.0,
+          controller.hasClients
+              ? (-(index * MediaQuery.of(context).size.height) +
+                      controller.position.pixels) /
+                  2
+              : 0.0,
+        ),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Image.asset(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
+
+class HomePageSection extends StatelessWidget {
+  const HomePageSection({
+    super.key,
+    required this.header,
+    this.content,
+  });
+
+  final String header;
+  final Widget? content;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: GlassContainer(
+        height: MediaQuery.of(context).size.height / 2.5,
+        width: MediaQuery.of(context).size.width / 2,
+        blur: 4,
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            textBaseline: TextBaseline.ideographic,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 4,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      header,
+                      style: const TextStyle(
+                        fontSize: 40.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w900,
+                      ),
+                      maxLines: 2,
+                    ),
+                    content ?? const LoremIpsum(),
+                  ],
+                ),
+              ),
+              const CustomNeoPopTileButton(label: 'Read More'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoremIpsum extends StatelessWidget {
+  const LoremIpsum({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      overflow: TextOverflow.ellipsis,
+      maxLines: 5,
+      softWrap: true,
+      style: TextStyle(
+        fontSize: 20.0,
+        color: Colors.white,
+        fontWeight: FontWeight.normal,
+      ),
+    );
+  }
+}
+
+class CustomNeoPopTileButton extends StatelessWidget {
+  final String label;
+  const CustomNeoPopTileButton({
+    super.key,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NeoPopTiltedButton(
+      isFloating: true,
+      color: Colors.white,
+      onTapUp: () => HapticFeedback.vibrate(),
+      onTapDown: () => HapticFeedback.vibrate(),
+      floatingDuration: const Duration(milliseconds: 500),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Text(
+          label,
+          style: const TextStyle(
+            fontSize: 18.0,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          margin: const EdgeInsets.all(5.0),
+          child: ClipRRect(
+              borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+              child: Stack(
+                children: <Widget>[
+                  Image.network(item, fit: BoxFit.cover, width: 1000.0),
+                  Positioned(
+                    bottom: 0.0,
+                    left: 0.0,
+                    right: 0.0,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [
+                            Color.fromARGB(200, 0, 0, 0),
+                            Color.fromARGB(0, 0, 0, 0)
+                          ],
+                          begin: Alignment.bottomCenter,
+                          end: Alignment.topCenter,
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
+                      child: Text(
+                        'Event No. ${imgList.indexOf(item) + 1}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+        ))
+    .toList();
+
+class UpcomingEventsSlider extends StatelessWidget {
+  const UpcomingEventsSlider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        autoPlay: true,
+        aspectRatio: 2.0,
+        enlargeCenterPage: true,
+      ),
+      items: imageSliders,
     );
   }
 }
